be08b03366454ec0be63b6d9f2b1c5fc2bc374fb
송진수 <jssong1592@gmail.com>
2022-02-03 17:15:18 +0900

    Add : Feb 03 (Thu)

---
+++ src/SWEA/D3/SWEA_1289/Solution.java
@@ -0,0 +1,33 @@
+package SWEA.D3.SWEA_1289;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc = Integer.parseInt(br.readLine());
+		
+		for (int t=0;t<tc;t++) {
+			String s = br.readLine();
+			String[] arr = s.split("");
+			int cnt = 0;
+			
+			for (int i=0;i<arr.length;i++) {
+				if (i==0) {
+					if (arr[0].equals("1")) {
+						cnt++;
+					}
+				} else if (!arr[i].equals(arr[i-1])) {
+					cnt++;
+				}
+			}
+			
+			System.out.printf("#%d "+ cnt +"%n",t+1);
+		}
+
+	}
+}

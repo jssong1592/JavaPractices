851f05197f5a89345c4a9e59a75b1563f84f7b55
송진수 <jssong1592@gmail.com>
2022-02-09 11:51:46 +0900

    Add : SWEA_3499 & SWEA_9229 & BOJ_2504

---
+++ src/BOJ/D3/SWEA_3499/Solution.java
@@ -0,0 +1,39 @@
+package BOJ.D3.SWEA_3499;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc=0;tc<T;tc++) {
+			
+			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			String[] arr1 = new String[N/2 + N%2];
+			String[] arr2 = new String[N/2];
+			
+			for (int i=0;i<arr1.length;i++) {
+				arr1[i] = st.nextToken();
+			}
+			
+			for (int i=0;i<arr2.length;i++) {
+				arr2[i] = st.nextToken();
+			}
+			
+			StringBuilder sb = new StringBuilder("#"+(tc+1)+" ");
+			for (int i=0;i<N/2;i++) {
+				sb.append(arr1[i]+" "+arr2[i]+" ");
+			}
+			if (N%2==1) sb.append(arr1[arr1.length-1]);
+			
+			System.out.println(sb);
+		}
+
+	}
+
+}
---
+++ src/SWEA/D3/SWEA_9229/Solution.java
@@ -0,0 +1,48 @@
+package SWEA.D3.SWEA_9229;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc=0;tc<T;tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			
+			
+			int[] arr = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+			
+			int max = 0;
+			int total = 0;
+			
+			for (int i=0;i<N-1;i++) {
+				for (int j=i+1;j<N;j++) {
+					total = arr[i] + arr[j];
+					if (total > max&&total <= M) max = total;
+				}
+			}
+			
+			
+			System.out.printf("#%d ",tc+1);
+			if (max==0) System.out.print(-1);
+			else System.out.print(max);
+			System.out.println();
+			
+			
+		}
+		
+	}
+
+}

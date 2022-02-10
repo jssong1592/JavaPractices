f9d1aa893563c2da2997f1c47d63f50404491bc8
송진수 <jssong1592@gmail.com>
2022-02-10 18:24:01 +0900

    Add : BOJ_2563

---
+++ src/BOJ/B1/BOJ_2563_색종이/Main.java
@@ -0,0 +1,41 @@
+package BOJ.B1.BOJ_2563_»öÁ¾ÀÌ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		int[][] map = new int[100][100];
+		int total = 0;
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			for (int j=x;j<x+10;j++) {
+				for (int k=y;k<y+10;k++) {
+					map[j][k] = 1;
+				}
+			}
+			
+		}
+		
+		for (int j=0;j<100;j++) {
+			for (int k=0;k<100;k++) {
+				total += map[j][k];
+			}
+		}
+		
+		System.out.println(total);
+
+	}
+
+}

54dd071da1899518447ee3fb005db7dd92333e0b
송진수 <jssong1592@gmail.com>
2022-02-14 16:13:24 +0900

    Add : BOJ_3040

---
+++ src/BOJ/B2/BOJ_3040_백설공주와일곱난쟁이/Main.java
@@ -0,0 +1,42 @@
+package BOJ.B2.BOJ_3040_归汲傍林客老蚌抄里捞;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+		
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] dwarf = new int[9];
+		
+		for (int i=0;i<9;i++) {
+			dwarf[i] = sc.nextInt();
+		}
+
+		outer:for (int i=(1<<1);i<(2<<8);i<<=1) {
+			for (int j=(1<<0);j<i;j<<=1) {
+				int total = 0;
+				ArrayList<Integer> list = new ArrayList<>();
+				int bit = (2<<8)-1-i-j;
+				for (int k=0;k<9;k++) {
+					if ((bit&(1<<k))!=0) {
+						list.add(dwarf[k]);
+						total += dwarf[k];
+					}
+				}
+				
+				if (total == 100&&list.size()==7) {
+					for (Integer d:list) {
+						System.out.println(d);
+					}
+					break outer;
+				}
+			}
+		}
+		
+		
+		
+	}
+
+}

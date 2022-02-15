95d629c19c8053567f7ad80028a2107290f1dabd
송진수 <jssong1592@gmail.com>
2022-02-15 23:33:36 +0900

    Add : BOJ_6416

--- src/BOJ/G5/BOJ_6416_트리인가/WrongMain.java
+++ src/BOJ/G5/BOJ_6416_트리인가/WrongMain.java
@@ -3,10 +3,9 @@ package BOJ.G5.BOJ_6416_트리인가;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main {
+public class WrongMain {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,8 +14,7 @@ public class Main {
 		
 		boolean flag = true;
 		HashMap<Integer,Integer> parentOf = new HashMap<>();
-		HashSet<Integer> nodes = new HashSet<>();
-		HashSet<int[]> vertexes = new HashSet<>();
+		
 		while(true) {
 			//줄 읽기
 			String line = br.readLine();
@@ -25,36 +23,26 @@ public class Main {
 			
 			for (String vertex:st) {
 				if (vertex.equals("0 0")) {//케이스 하나가 끝났으므로 결과 처리 후 플래그 초기화
-					if (nodes.size()==0||(flag&&countRoot(nodes,parentOf)==1&&nodes.size()-vertexes.size()==1))
-						System.out.println("Case "+(tc)+" is a tree.");
+					if (parentOf.size()==0||(flag&&countRoot(parentOf)==1))System.out.println("Case "+(tc)+" is a tree.");
 					else System.out.println("Case "+(tc)+" is not a tree.");
 					
 					//케이스 초기화하고 다음 줄 읽고 넘어가기
 					tc++;
-					
-					flag = true;
 					parentOf = new HashMap<>();
-					nodes = new HashSet<>();
-					vertexes = new HashSet<>();
-					
 					line = br.readLine();
+					flag = true;
 					
 					break;
 				} else {
 					StringTokenizer st2 = new StringTokenizer(vertex);
 					int parent = Integer.parseInt(st2.nextToken());
 					int child = Integer.parseInt(st2.nextToken());
-					
-					nodes.add(parent);
-					nodes.add(child);
-					vertexes.add(new int[] {parent,child});
-					
-					// 수정) 트리일 경우
-					// ★. 노드가 없어도 트리다
-					// 1. 루트가 노드가 하나
-					// 2. 정점 갯수 - 간선 갯수 == 1
-					// 3. 들어오는 간선의 갯수가 2 이상인 노드가 있는지(child의 부모가 이미 존재)
-					if (parent==child||parentOf.get(child)!=null) {
+					// 트리가 아닌 경우
+					// 1. parent와 child가 같을 때
+					// 2. child의 부모가 이미 존재할 때
+					// 3. 노드가 순환할 때(조상을 자식으로 만드려고 할 때)
+					// 4. 루트 노드가 2개 이상일 때
+					if (parent==child||parentOf.get(child)!=null||isAncestor(child,parent,parentOf)) {
 						flag = false;
 					} else {
 						parentOf.put(child, parent);
@@ -63,17 +51,38 @@ public class Main {
 			}
 			
 			//케이스 끝내고 읽은 다음 줄이 -1 -1이면 완전히 끝내기
-			if (line.equals("-1 -1")) return;
+			if (line.equals("-1 -1")) break;
 			
 		}
 		
 
 	}
-
-	static int countRoot(HashSet<Integer> nodes, HashMap<Integer,Integer> parentOf) {
+	
+	static boolean isAncestor(int child, int parent, HashMap<Integer,Integer> parentOf) {
+		while(parentOf.get(parent)!=null) {
+			if (parentOf.get(parent)==child) {
+				return true;
+			}
+			parent = parentOf.get(parent);
+			
+		}
+		return false;
+	}
+	
+	static int countRoot(HashMap<Integer,Integer> parentOf) {
 		int cnt = 0;
-		for (int node:nodes) {
-			if (parentOf.get(node)==null) cnt++;
+		int root = -1;
+		for(Integer child:parentOf.keySet()) {
+			int parent = parentOf.get(child);
+			while(parentOf.get(parent)!=null) {
+		
+				parent = parentOf.get(parent);
+				
+			}
+			if (root!=parent) {
+				root = parent;
+				cnt++;
+			}
 		}
 		return cnt;
 	}
--- src/BOJ/S1/BOJ_1074_Z/Main.java
+++ src/BOJ/S1/BOJ_1074_Z/Main.java
@@ -2,7 +2,7 @@ package BOJ.S1.BOJ_1074_Z;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,24 +15,24 @@ public class Main {
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 		
-		ArrayList<Integer> list = new ArrayList<>(); //4분면 단위 좌표 저장할 arrayList
+		Stack<Integer> stack = new Stack<>(); //4분면 단위 좌표 저장할 arrayList
 		
 		int length = (1<<N); //2의 N제곱부터 시작
 		
 		while (length>1) {
 			length = length/2;
 			if (r<length) { //찾고자 하는 행이 길이의 절반보다 작으면
-				if (c<length) list.add(0);
+				if (c<length) stack.push(0);
 				else { //찾고자 하는 열이 길이의 절반보다 크면
-					list.add(1);
+					stack.push(1);
 					c = c-length; //열의 상대 위치 조정
 				}
 			} else { //찾고자 하는 행이 길이의 절반보다 크면
 				if (c<length) {
-					list.add(2);
+					stack.push(2);
 				}
 				else {
-					list.add(3);
+					stack.push(3);
 					c = c-length; //열의 상대 위치 조정
 				}
 				r = r-length; //행의 상대 위치 조정
@@ -40,11 +40,12 @@ public class Main {
 		}
 		
 		int ans = 0;
-		int area = (int) Math.pow(4, N-1);
-		for (Integer i:list) {
-			ans+= i * area;
-			area /= 4;
+		int area = 1;
+		while (!stack.isEmpty()) {
+			ans += stack.pop() * area;
+			area *= 4;
 		}
+		
 
 		System.out.println(ans);
 	}

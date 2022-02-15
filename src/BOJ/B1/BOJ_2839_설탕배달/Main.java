766ca70394fef69ab936a0e5e1b8dd960443208e
송진수 <jssong1592@gmail.com>
2022-02-15 13:21:00 +0900

    Add : BOJ_2839

---
+++ src/BOJ/B1/BOJ_2839_설탕배달/Main.java
@@ -0,0 +1,25 @@
+package BOJ.B1.BOJ_2839_º≥≈¡πË¥ﬁ;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int a = N/5;
+		int b = N%5;
+		
+		while (b%3!=0) {
+			if (a==0) break;
+			a--;
+			b+=5;
+		}
+		
+		if (b%3==0) System.out.println(a+(b/3));
+		else System.out.println(-1);
+	}	
+}
+	
---
+++ src/BOJ/G5/BOJ_6416_트리인가/Main.java
@@ -0,0 +1,10 @@
+package BOJ.G5.BOJ_6416_트리인가;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
